047f3c3be0296bbb274db5e4a56b853dbb1f41fe
Igor Andrade Nepomuceno <125659508+IGOR1630@users.noreply.github.com>
2023-07-07 09:54:11 -0300

    Add files via upload

---
+++ balanceLine/balanceLine.h
@@ -0,0 +1,16 @@
+/*
+ * balanceLine.h
+ *
+ *  Modified on: Jul, 2023
+ *      Author: raphael oliveira
+ */
+
+
+#include <stdio.h> /* define FILE */
+#include <stdlib.h>
+
+void balanceLine(FILE *fMestre, FILE *fTransacao, FILE *fNovoMestre, FILE *fErro){
+	//TODO
+	//implementar o algoritmo do Balance Line
+}
+
